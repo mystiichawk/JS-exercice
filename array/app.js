ec9ba2d89e9d2bb4210094658f957fbaecc3a2d4
Mickael <polmickael3@gmail.comgit config --global user.name>
2017-07-05 11:56:01 +0200

    exo 5 afficher "août"

--- array/app.js
+++ array/app.js
@@ -3,6 +3,7 @@ var mois = ["janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "ao
 console.log (mois [2]);
 console.log (mois [5]);
 console.log (mois [10]);
+console.log (mois [7] = "août");
 
 
 
