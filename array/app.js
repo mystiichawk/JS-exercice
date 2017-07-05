876c1676f585128a9fba56677a9eeba575b998d5
Mickael <polmickael3@gmail.comgit config --global user.name>
2017-07-05 11:23:33 +0200

    fin exercice 1 et ajout de l'index.html

--- array/app.js
+++ array/app.js
@@ -1,1 +1,5 @@
-var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+var mois = ["janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"];
+
+
+
+
